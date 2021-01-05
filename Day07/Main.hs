21759e4f7642e3e722a4fd5469aa77d90f405dcd
Drew Haven <drew.haven@gmail.com>
2021-01-05 13:41:28 -0800

    Day 7: Update harness

---
+++ Day07/Main.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Text.Printf
+
+import Exercise
+import Types
+import Parser
+import Graph
+
+main :: IO ()
+main = do
+    rules <- parseInput bagRules
+    let graph = makeGraphFromRules rules
+    countContainers <- runExercise "Part 1" (length . allContainers (Bag "shiny" "gold")) graph
+    printf "Total containers for 'shiny gold': %d\n" countContainers
+    countContents <- runExercise "Part 2" (countContents (Bag "shiny" "gold")) graph
+    printf "Total contents for 'shiny gold': %d" countContents
+
