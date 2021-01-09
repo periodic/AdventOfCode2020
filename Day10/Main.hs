6d05c4558cefb8db9b1809bdc16306b3a8780eb6
Drew Haven <drew.haven@gmail.com>
2021-01-08 22:44:24 -0800

    Day 10: Optimizations

--- Day10/Main.hs
+++ Day10/Main.hs
@@ -1,51 +1,51 @@
 module Main where
 
 import Data.Attoparsec.Text as P
+import qualified Data.IntMap as M
 import Data.List as L
-import Data.IntMap as M
-import Text.Printf
-
 import Exercise
+import Text.Printf
 
 main :: IO ()
 main = do
-    sortedJoltages <- parseInput (L.sort . addFirstAndLast <$> P.sepBy P.decimal P.endOfLine)
-    result1 <- runExercise "Part 1" (uncurry (*) . count1and3 . differences) sortedJoltages
-    putStr "ones * threes = "
-    print result1
-    result2 <- runExercise "Part 2" totalCombinations sortedJoltages
-    printf "total combinations = %d\n" result2
+  sortedJoltages <- parseInput (L.sort . addFirstAndLast <$> P.sepBy P.decimal P.endOfLine)
+  result1 <- runExercise "Part 1" (uncurry (*) . count1and3 . differences) sortedJoltages
+  putStr "ones * threes = "
+  print result1
+  result2 <- runExercise "Part 2" totalCombinations sortedJoltages
+  printf "total combinations = %d\n" result2
 
 addFirstAndLast :: [Int] -> [Int]
 addFirstAndLast joltages =
-    0 : (maximum joltages + 3) : joltages
+  0 : (maximum joltages + 3) : joltages
+
+differences :: [Int] -> [Int]
+differences sortedJoltages =
+  zipWith (-) (tail sortedJoltages) sortedJoltages
 
 count1and3 :: [Int] -> (Int, Int)
-count1and3 values =
-    let
-        ones = L.length . L.filter (== 1) $ values
-        threes = L.length . L.filter (== 3) $ values
-    in
-        (ones, threes)
-
--- Input should be sorted.
-differences :: [Int] -> [Int]
-differences joltages =
-    zipWith (-) (tail joltages) joltages
+count1and3 =
+  foldr addDiff (0, 0)
+  where
+    addDiff x (ones, threes) =
+      case x of
+        1 ->
+          (ones + 1, threes)
+        3 ->
+          (ones, threes + 1)
+        _ ->
+          (ones, threes)
 
 totalCombinations :: [Int] -> Int
 totalCombinations sortedJoltages =
-        snd . M.findMax $ L.foldl (flip addJoltage) M.empty sortedJoltages
-
-addJoltage :: Int -> IntMap Int -> IntMap Int
-addJoltage j combinationMap =
-    let
-        combinations = sum . L.map (findWithDiff combinationMap j) $ [1..3]
-    in
-        if j == 0
-            then M.insert j 1 combinationMap
-            else M.insert j combinations combinationMap
-
-findWithDiff :: IntMap Int -> Int -> Int -> Int
-findWithDiff combinationMap j diff =
-    M.findWithDefault 0 (j - diff) combinationMap
+  combinations M.! maximum sortedJoltages
+  where
+    combinations =
+      M.insert 0 1
+        . M.mapWithKey
+          ( \key _ ->
+              sum (L.map (\diff -> M.findWithDefault 0 (key - diff) combinations) [1 .. 3])
+          )
+        . M.fromList
+        . L.map (,1)
+        $ sortedJoltages
--- Day10/README.md
+++ Day10/README.md
@@ -12,4 +12,43 @@ differences joltages =
 
 From there you can just just fold it into some counts, build a map out of it, or even just filter it twice.  Either way, it's pretty fast.
 
-The se
+The second part, the main thing to realize is that you have to memoize as you go and you can't just brute-force it repeatedly.  My first version of this built up a map with a few recursive functions.  This took about 5µs.  I switched over to a terser lazy map, similar to Day 7, but that increased the runtime to 9µs.  However, it's much better looking, so I'm leaving it at that.
+
+## Optimizations
+
+There are a few small optimizations I can make.  For example, in the first part I was filtering twice.  It's possible to do that in one pass, which halves the time required, getting it under a microsecond.
+
+On the second part, I already mentioned that it's possible to squeeze out a little bit more performance by hand-rolling the memoization, but it's a bit more wordy than I think it worth it for only a 2x speed-up.
+
+### Final Benchmark
+
+```
+Parsing input...
+benchmarking...
+time                 4.411 μs   (4.397 μs .. 4.436 μs)
+                     0.999 R²   (0.999 R² .. 1.000 R²)
+mean                 4.496 μs   (4.447 μs .. 4.558 μs)
+std dev              183.9 ns   (127.1 ns .. 258.4 ns)
+variance introduced by outliers: 53% (severely inflated)
+
+================================================================================
+Running Part 1...
+benchmarking...
+time                 847.2 ns   (844.0 ns .. 850.9 ns)
+                     1.000 R²   (1.000 R² .. 1.000 R²)
+mean                 849.9 ns   (845.7 ns .. 863.0 ns)
+std dev              21.62 ns   (4.984 ns .. 41.98 ns)
+variance introduced by outliers: 34% (moderately inflated)
+
+ones * threes = 1625
+================================================================================
+Running Part 2...
+benchmarking...
+time                 10.19 μs   (10.13 μs .. 10.26 μs)
+                     0.999 R²   (0.998 R² .. 1.000 R²)
+mean                 10.23 μs   (10.16 μs .. 10.36 μs)
+std dev              286.9 ns   (93.66 ns .. 484.7 ns)
+variance introduced by outliers: 32% (moderately inflated)
+
+total combinations = 3100448333024
+```
